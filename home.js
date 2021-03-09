628a2b029b7b6a5e0bff9531f2f6e22caf74b4b1
Gary Burger <rburger27@gmail.com>
2021-03-08 20:42:46 -0600

    First Commit

---
+++ home.js
@@ -0,0 +1,50 @@
+console.log("Hello");
+console.log("World");
+// alert("Yo");
+
+//variables
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+// Manioulate DOM with Javascript
+/* . . . It's a fancy was of saying change html with Javascript */
+
+
+ //vvar age = prompt('What is your age?');
+// console.log(age);
+
+// document.getElementById('someText').innerHTML = age;
+
+// Numbers in JavaScript
+
+var num1 = 10;
+
+// Increment num1 by 1
+
+num1++;
+
+console.log(num1);
+
+// decrement num1 by 1
+
+num1--;
+
+console.log(num1);
+
+
+//Divide /, Multiply *, and get remainders %
+
+console.log(num1/5);
+console.log(num1*5);
+console.log(num1%5);
+
+// Increment or dectrement by any number you want
+
+num1 += 10;
+console.log(num1);
+
+num1 -= 10;
+console.log 
